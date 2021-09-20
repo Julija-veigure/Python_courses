20b3948d2c9417e1f67727b8a7f960966421440b
Julija-veigure <julija.veigure@gmail.com>
2021-09-20 18:54:38 +0300

    First exercise

---
+++ 1_day/Homework_1.py
@@ -0,0 +1,13 @@
+print("Day 1 Homework")
+# Write the following code in Python:
+#
+# print your name 5 times
+print ("Julija\n" * 5)
+# print Bananananana using "Ba" once and "na" 5 times
+print("Ba" + "na" * 5)
+# print the number of seconds in a year calculated from months, days, minutes, seconds
+print(365*24*60*60)
+# how much a googol is that is 10 to the power of the 100th
+print(10**100)
+# how much will your 1000 Euro investment be worth after 12 years of 6% yearly interest?
+print(1000 + (1000 * 1.06*12))
